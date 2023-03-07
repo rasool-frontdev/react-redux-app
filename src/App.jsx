f3e2629ab4a4d441ec3835929b2fe2a1cfa94919
Gasirbek.T <gasirbek_ernazaruli3112@mail.ru>
2023-03-07 17:22:33 +0500

    Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import "./styles/style.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>app</h1>
+      <div className="container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
     </>
   );
 }
