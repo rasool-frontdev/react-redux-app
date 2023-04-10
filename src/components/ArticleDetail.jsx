3fff68f30660bdbabc8c08ea5ae0b8e9b2b963db
Gasirbek.T <gasirbek_ernazaruli3112@mail.ru>
2023-04-10 18:54:51 +0500

    updated article ui

--- src/components/ArticleDetail.jsx
+++ src/components/ArticleDetail.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import ArticleService from "../service/article";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   getArticleDetailFailure,
   getArticleDetailStart,
   getArticleDetailSuccess,
 } from "../slice/article";
-
+import moment from "moment";
+import Loader from "../ui/loader";
 const ArticleDetail = () => {
   const { slug } = useParams();
   const dispatch = useDispatch();
+  const { ArticleDetail, isLoading } = useSelector((state) => state.article);
 
   const getArticleDetail = async () => {
     dispatch(getArticleDetailStart());
@@ -18,7 +20,7 @@ const ArticleDetail = () => {
       const response = await ArticleService.getArticleDetail(slug);
       dispatch(getArticleDetailSuccess(response.article));
     } catch (error) {
-      dispatch(getArticleDetailFailure());
+      dispatch(getArticleDetailFailure(error));
     }
   };
 
@@ -26,7 +28,32 @@ const ArticleDetail = () => {
     getArticleDetail();
   }, [slug]);
 
-  return <div>id: {slug} </div>;
+  return isLoading ? (
+    <Loader />
+  ) : (
+    ArticleDetail !== null && (
+      <div>
+        <div className="p-5 mb-4 bg-light rounded-3">
+          <div className="container-fluid py-5">
+            <h1 className="display-5 fw-bold">{ArticleDetail.title}</h1>
+            <p className="col-md-8 fs-4">{ArticleDetail.description}</p>
+            <div className="d-flex gap-3">
+              <p>
+                {" "}
+                <span className="fw-bold text-muted">Created at:</span>{" "}
+                {moment(ArticleDetail.createdAt).format("DD MM, YYYY")}
+              </p>
+            </div>
+            <div>{ArticleDetail.body}</div>
+          </div>
+          <div className="d-flex justify-content-end gap-1">
+            <span className="fw-bold text-muted">Author:</span>
+            <p className="fw-bold fs-italic">{ArticleDetail.author.username}</p>
+          </div>
+        </div>
+      </div>
+    )
+  );
 };
 
 export default ArticleDetail;
